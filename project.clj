b66b09d5a7535670a2ed3b320efc73c24cb0652d
ilevd <ilevd@mail.ru>
2022-11-22 13:48:45 +0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hugsql-inline-plugin "0.1.0"
+(defproject org.clojars.ilevd/hugsql-inline-plugin "0.1.0"
   :description "HugSQL plugin that allows inline sqlvec parameters to SQL string"
   :url "https://github.com/ilevd/hugsql-inline-plugin"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
